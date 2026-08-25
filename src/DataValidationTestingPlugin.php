bcf66ffa94daa4c492a1b98850c5797fc68a4685
Neluxx <fabian.arndt96@proton.me>
2026-08-25 11:16:09 +0200

    Add DataValidationTestingPlugin class

---
+++ src/DataValidationTestingPlugin.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace DataValidationTesting;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * CakePHP Data Validation Testing Plugin
+ */
+class DataValidationTestingPlugin extends BasePlugin
+{
+}
